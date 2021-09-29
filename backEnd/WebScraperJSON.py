a56fb741c02b0752fa707be4ce605a3982bbe2d7
Tyler Ma <tyler@atlma.com>
2021-09-29 19:31:27 -0400

    just stuff I did during the meeting

--- backEnd/WebScraperJSON.py
+++ backEnd/WebScraperJSON.py
@@ -8,9 +8,16 @@ data = resp.json()["product"]
 def itemName():
     return data["title"]
 
+# getting the options like size
+def itemOptions():
+    options = []
+    for item in (data["variants"]):
+        if item["option1"] not in options:
+            options.append(item["option1"])
+    return options
 
-options = []
-for item in (data["variants"]):
-    if item["option1"] not in options:
-        options.append(item["option1"])
-print(options)
+# calling and printing
+print("Name:" + itemName())
+print("Available sizes: ", end='')
+for itemOption in itemOptions():
+    print(itemOption + ", ", end='')
