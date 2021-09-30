93d7a610925035e0d0ffbb0a1bb4e3b8a302aaaa
nickundin <nickundin@gmail.com>
2021-09-30 17:36:04 -0400

    Added products page with fake data

--- backEnd/app.py
+++ backEnd/app.py
@@ -2,10 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='../frontEnd/templates')
 
+# simulates data in the database for now
+products = [
+    {
+        'name': 'Keyboard',
+        'price': 100,
+        'description': 'A funny description'
+    },
+    {
+        'name': 'Shoes',
+        'price': 200,
+        'description': 'Test'
+    }
+]
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/products')
+def displayProducts():
+    return render_template('products.html', products=products)
 
 
 if __name__ == "__main__":
