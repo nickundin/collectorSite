737fcd5171238f6f27432430cb4f77662df0303f
nickundin <nickundin@gmail.com>
2021-10-08 18:32:01 -0400

    Created a basic database and a dbTODO list

--- backEnd/app.py
+++ backEnd/app.py
@@ -1,6 +1,32 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='../frontEnd/templates')
+
+# START OF DATABASE STUFF
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # unique id for the product
+    image = db.Column(db.String(20), nullable=False,
+                      default='default.jpg')
+# ALL OF THE STRINGS HAVE A MAX LENGTH OF 20 FOR NOW. WILL CHANGE LATER
+    title = db.Column(db.String(20), unique=True, nullable=False)
+    price = db.Column(db.Float)
+    condition = db.Column(db.String(20))
+    sellerInfo = db.Column(db.String(20))
+    availability = db.Column(db.String(20))
+    returnPolicy = db.Column(db.String(20))
+
+    # describes how the object will be printed out as a string
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.price}', '{self.condition}'"
+
+# END OF DATABASE STUFF
+
 
 # simulates data in the database for now
 products = [
