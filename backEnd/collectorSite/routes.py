a2e37b68f15b63f0e1b8b3ef23111a18aaf70f45
nickundin <nickundin@gmail.com>
2021-10-09 13:12:43 -0400

    Queried products from db so they show up on the products page

--- backEnd/collectorSite/routes.py
+++ backEnd/collectorSite/routes.py
@@ -1,23 +1,27 @@
 from flask import render_template
 from collectorSite import app
-# from collectorSite.models import Product
+from collectorSite.models import Product
+
+# gets the actual products from the database
+products = Product.query.all()
 
 # simulates data in the database for now
-products = [
-    {
-        'name': 'Keyboard',
-        'price': 100,
-        'description': 'A funny description'
-    },
-    {
-        'name': 'Shoes',
-        'price': 200,
-        'description': 'Test'
-    }
-]
+# products = [
+#     {
+#         'name': 'Keyboard',
+#         'price': 100,
+#         'description': 'A funny description'
+#     },
+#     {
+#         'name': 'Shoes',
+#         'price': 200,
+#         'description': 'Test'
+#     }
+# ]
 
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
