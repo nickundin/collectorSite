c4342dad9bdb51ed1d83443f7e8dae05ed0ca20b
Tyler Ma <tyler@atlma.com>
2021-11-10 18:43:15 -0500

    update to auth

--- backEnd/collectorSite1/auth.py
+++ backEnd/collectorSite1/auth.py
@@ -16,14 +16,30 @@ def index():
 
         new_user = User(email=email, password=generate_password_hash(
             password, method='sha256'), username=username)
-        db.session.add(new_user)
-        db.session.commit()
-        print("New user created")
-        # TODO inside of ulr_for insert the home page route
-        return redirect(url_for())
+        user = User.query.filter_by(email=email).first()
+        if user:
+            print("User already exists")  # TODO change to flash
+        else:
+            db.session.add(new_user)
+            db.session.commit()
+            print("New user created")  # TODO change to flash
+            # TODO inside of ulr_for insert the home page route
+            return redirect(url_for())
     return render_template('index.html')  # TODO insert the template for signup
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def index():
+    if requests.method == 'POST':
+        email = requests.form.get('email')
+        password = requests.form.get('username')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                print("Logged in successfully")  # TODO change to flash
+            else:
+                print("Incorrect password")  # TODO change to flash
+        else:
+            print("User does not exist")  # TODO change to flash
+
     return render_template('index.html')  # TODO insert the template for login
